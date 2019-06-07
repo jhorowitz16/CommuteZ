6c13de31622c6facb86b873d6dd94f92f950fdc9
Joshua Horowitz <joshua.horowitz@berkeley.edu>
2019-06-06 23:30:06 -0700

    add dummy buttons and style

--- commutez-app/src/components/Dashboard.jsx
+++ commutez-app/src/components/Dashboard.jsx
@@ -7,10 +7,21 @@ class Dashboard extends Component {
   static propTypes = {
   };
 
-    render() {
+  render() {
     return (
-      <div className='clock'>
-        Dashboard
+      <div className='dashboard'>
+        <div className='dashboard__buttons'>
+          <div><button><span>Leaving Home</span></button></div>
+          <div><button><span>At MV Caltrain</span></button></div>
+          <div><button><span>At SF Caltrain</span></button></div>
+          <div><button><span>At Work</span></button></div>
+        </div>
+
+        <div className='dashboard__action'>
+          No action
+        </div>
+
+
       </div>
     );
   }
